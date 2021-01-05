e39282fc450439b3ccde62fbcddebed26f2ab9a1
Marco Palazzetti <marco.palazzetti@me.com>
2021-01-05 20:51:23 +0100

    Restored wrong uncommit

--- testscript.py
+++ testscript.py
@@ -4,6 +4,8 @@ from palazzetti_local_api import Palazzetti, PalDiscovery
 
 _LOGGER = logging.getLogger(__name__)
 
+TEST_IP = "192.168.1.130"
+
 def main():
     api_discovery=PalDiscovery()
     loop = asyncio.get_event_loop()
@@ -12,15 +14,42 @@ def main():
     found_ips=loop.run_until_complete(api_discovery.discovery())
     print(found_ips)
 
-    if found_ips:
-        use_ip=found_ips[0]
-        #use_ip="192.168.1.133"
-
-        print(f"Using IP: {use_ip}")
-        api=Palazzetti(use_ip)
-    else:
+    if not found_ips:
         print("No ConnBox found")
         return
+
+    use_ip=found_ips[0]
+    #use_ip="192.168.1.133"
+    print(f"Using IP: {use_ip}")
+    api=Palazzetti(use_ip)
+    loop.run_until_complete(api.async_get_alls())
+    #loop.close()
+    print(f"{api.get_datas()}")
+    #key volutamente errata
+    print(api.get_key('MACive'))
+    #diverse forme di print
+    print(api.get_key('MAC'))
+    print(f"{api.get_key('STATUS')} {api.get_key('MAC')}")
+    loop.run_until_complete(api.async_get_gen('GET STDT'))
+    loop.close()
+    print(f"{api.get_datas()}")
+
+def main2():
+    api_discovery=PalDiscovery()
+    loop = asyncio.get_event_loop()
+
+    print("Chiamata diretta:")
+    use_ip=TEST_IP
+    is_IP_OK=loop.run_until_complete(api_discovery.checkIP_HTTP(use_ip))
+    print(f"From checkIP_HTTP: {is_IP_OK}")
+
+    if not is_IP_OK:
+        print("No ConnBox found")
+        return
+
+    #use_ip="192.168.1.133"
+    print(f"Using IP: {use_ip}")
+    api=Palazzetti(use_ip)
 
     loop.run_until_complete(api.async_get_alls())
     #loop.close()
@@ -34,6 +63,67 @@ def main():
     loop.close()
     print(f"{api.get_datas()}")
 
+def main3():
+    api_discovery=PalDiscovery()
+    loop = asyncio.get_event_loop()
+
+    print("Chiamata diretta:")
+    use_ip = TEST_IP
+    is_IP_OK=loop.run_until_complete(api_discovery.checkIP_UDP(use_ip))
+    print(f"From checkIP_UDP {is_IP_OK}")
+
+    if not is_IP_OK:
+        print("No ConnBox found via UDP, checking via HTTP...")
+        is_IP_OK=loop.run_until_complete(api_discovery.checkIP_HTTP(use_ip))
+        print(f"From checkIP_HTTP {is_IP_OK}")
+        if not is_IP_OK:
+            print("No ConnBox found")
+            return
+    
+    #use_ip="192.168.1.133"
+    print(f"Using IP: {use_ip}")
+    api=Palazzetti(use_ip)
+    
+    loop.run_until_complete(api.async_get_alls())
+    #loop.close()
+    print(f"{api.get_datas()}")
+    #key volutamente errata
+    print(api.get_key('MACive'))
+    #diverse forme di print
+    print(api.get_key('MAC'))
+    print(f"{api.get_key('STATUS')} {api.get_key('MAC')}")
+    loop.run_until_complete(api.async_get_gen('GET STDT'))
+    loop.close()
+    print(f"{api.get_datas()}")
+
+def main4():
+    api_discovery=PalDiscovery()
+    loop = asyncio.get_event_loop()
+
+    print("Chiamata diretta:")
+    use_ip = TEST_IP
+    is_IP_OK=loop.run_until_complete(api_discovery.checkIP(use_ip))
+    print(f"From checkIP: {is_IP_OK}")
+
+    if not is_IP_OK:
+        print("No ConnBox found")
+        return
+
+    #use_ip="192.168.1.133"
+    print(f"Using IP: {use_ip}")
+    api=Palazzetti(use_ip)
+
+    loop.run_until_complete(api.async_get_alls())
+    #loop.close()
+    print(f"{api.get_datas()}")
+    #key volutamente errata
+    print(api.get_key('MACive'))
+    #diverse forme di print
+    print(api.get_key('MAC'))
+    print(f"{api.get_key('STATUS')} {api.get_key('MAC')}")
+    loop.run_until_complete(api.async_get_gen('GET STDT'))
+    loop.close()
+    print(f"{api.get_datas()}")
 
 if __name__ == "__main__":
-    main()
+    main4()
